d87ab2d66bc2d250ec626bf8ee7bbaff1ce93043
MoMo97X <72156485+MoMo97X@users.noreply.github.com>
2026-02-10 15:11:58 +0100

    finishing the Currency Convertor webApp

--- Currency-Convertor/js/currency.js
+++ Currency-Convertor/js/currency.js
@@ -1,22 +1,31 @@
-/* Selecting target element */
-let amount = document.querySelector(".amout");
+let amount = document.querySelector(".amount");
 let from = document.querySelector(".from");
 let to = document.querySelector(".to");
-let covBtn = document.querySelector(".btn");
 let box = document.querySelector(".box");
+let covBtn = document.querySelector(".btn");
+let data;
 
-function getApi() {
-  fetch(
-    `https://v6.exchangerate-api.com/v6/840a13dde2ef3e6f00a4aabf/latest/${from}`,
-  )
-    .then((res) => response.json)
-    .then((data) => console.log(data));
-}
-
-function convert(d) {
-  if (amount && from && to) {
-  } else {
+async function fetchApi() {
+  try {
+    let response = await fetch(
+      `https://v6.exchangerate-api.com/v6/840a13dde2ef3e6f00a4aabf/latest/USD`,
+    );
+    data = await response.json();
+  } catch {
+    box.innerHTML = `Failed to fetch exchange rates ${error}`;
   }
 }
 
-covBtn.addEventListener("click", convert);
+fetchApi();
+
+covBtn.addEventListener("click", async () => {
+  if (!amount.value) {
+    box.innerHTML = "please enter an amount";
+    return;
+  }
+
+  let amount_Usd = amount.value / data.conversion_rates[from.value];
+  let result = amount_Usd * data.conversion_rates[to.value];
+
+  box.innerHTML = ` ${amount.value} ${from.value} = ${result.toFixed(2)} ${to.value}`;
+});
